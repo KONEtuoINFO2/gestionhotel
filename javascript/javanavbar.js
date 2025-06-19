36d94c845a040992d3826f41006898125bd2d115
KONEtuoINFO2 <nalourgotuo6@gmail.com>
2025-06-19 03:23:02 +0100

    js de vavbar

---
+++ javascript/javanavbar.js
@@ -0,0 +1,7 @@
+
+  const menuBtn = document.querySelector('.menu-toggle');
+  const navbar = document.querySelector('.navbar');
+
+  menuBtn.addEventListener('click', () => {
+    navbar.classList.toggle('show');
+  });
